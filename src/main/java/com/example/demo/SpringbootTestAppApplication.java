e724154ff90eca5f24981a3c36dbdd9aab42244e
venka <venka@DESKTOP-RLD91KR>
2021-01-30 23:30:20 +0530

    WH5125 first time commit

---
+++ src/main/java/com/example/demo/SpringbootTestAppApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTestAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootTestAppApplication.class, args);
+		System.out.println("This is spring boot application..........");
+	}
+
+}
